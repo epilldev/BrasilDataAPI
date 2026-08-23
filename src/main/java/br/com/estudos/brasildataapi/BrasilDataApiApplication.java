f7aac299dbd7532a50fe58d7a381a677bfc420b1
Felipe Joaquim <felipejo27@gmail.com>
2026-08-23 11:51:42 -0300

    chore: inicializa projeto Spring Boot
    Cria a estrutura inicial do projeto Brasil Data API
    utilizando Java 17, Spring Boot e Maven.
    
    Prepara a base do projeto para as futuras integrações
    com a API do IBGE, REST, gRPC e Protocol Buffers.

---
+++ src/main/java/br/com/estudos/brasildataapi/BrasilDataApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.estudos.brasildataapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrasilDataApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BrasilDataApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/estudos/brasildataapi/BrasilDataApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.estudos.brasildataapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrasilDataApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
